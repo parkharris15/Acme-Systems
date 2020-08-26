f1b22f91214fd63ab86e9226ec43dc4e02f6d692
parkharris15 <69918902+parkharris15@users.noreply.github.com>
2020-08-26 01:33:20 -0400

    Update HelloDolly.java

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,6 +3,7 @@ package ver1;
 public class HelloDolly {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Dolly from Eclipse");
 		System.out.println("Hello Dolly from Eclipse Again");
 		
 
